004bd92261f33d8c59684da3f7839536907e068b
Jonathan Miller <jonathan.d.mllr@gmail.com>
2022-04-05 13:03:31 -0400

    added several API fetch functions. Still need to build parameter input functionality

--- jsAPI.js
+++ jsAPI.js
@@ -16,12 +16,12 @@ $(".search-bar").submit(function (event) {
 
 //fetch data from OpenWeather API based on search bar input
 function fetchWeatherData(city) {
-  var queryURL =
+  var queryWeatherURL =
     "https://api.openweathermap.org/data/2.5/weather?q=" +
     city +
     "&appid=" +
-    APIKey;
-  fetch(queryURL)
+    weatherAPI;
+  fetch(queryWeatherURL)
     .then(function (response) {
       return response.json();
     })
@@ -29,16 +29,33 @@ function fetchWeatherData(city) {
       convertDate(data);
       currentDay.innerHTML = data.name + " " + todayDate;
       getForecast(data.coord.lat, data.coord.lon);
-      //need to pass in function to retrieve Ticket Master API information based on lat and Lon
-      fetchMusicData(data.coord.lat, data.coord.lon);
       console.log(data);
     });
 }
 
-function fetchMusicData(lat, lon) {
+//generates music query URL
+function createMusicQuery() {
+  var queryMusicURL =
+    "https://app.ticketmaster.com/discovery/v2/events?apikey=" + musicAPI;
   
-}
+  //will take parameter inputs to create query URL.
+  //if loop here.
+  
 
+  //fetches TicketMaster API data based on inputed parameters
+  fetch(queryMusicURL)
+    .then(function (response) {
+      if (response.ok) {
+        response.json().then(function (data) {
+          console.log(data);
+          //function calls to display queried data here
+        });
+      }
+    })
+    .catch(function (error) {
+      console.log("Error: " + error);
+    })
+}
   
 //function the retrieves forecast data from openweather API based on lat & lon
 function getForecast(lat, lon) {
@@ -49,12 +66,14 @@ function getForecast(lat, lon) {
     lon +
     "&exclude=minutely,hourly,alerts&appid=c05979fd50e5119c43cbd3ab81fe852f&units=imperial";
 
+  //uses the open weather api to fetch weather data
   fetch(getForecastUrl)
     .then(function (response) {
       if (response.ok) {
         response.json().then(function (data) {
           console.log(data);
-          displayWeather(data);
+          //displayWeather function call here
+          //displayWeather(data);
         });
       }
     })
