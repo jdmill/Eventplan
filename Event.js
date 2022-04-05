9a3bb11481508995877d53abf9d1064bc08bea2f
sebastian Chunley <101592116+chuseblok@users.noreply.github.com>
2022-04-05 11:28:29 -0400

    Add files via upload

---
+++ Event.js
@@ -0,0 +1,21 @@
+var concertButn = document.getElementById("get-concert");
+var festivalButn = document.getElementById("get-festival");
+var allAgesButn = document.getElementById("get-ages");
+var afternoonButn = document.getElementById("get-afternoon");
+var nightButn = document.getElementById("get-night");
+var searchButn = document.getElementById("get-search");
+var dateButn = document.getElementById("get-date");
+
+concertButn.addEventListener("click", Concert);
+
+festivalButn.addEventListener("click", Festival);
+
+allAgesButn.addEventListener("click", All-Ages);
+
+afternoonButn.addEventListener("click", Afternoon);
+
+nightButn.addEventListener("click", Night);
+
+searchButn.addEventListener("click", Search);
+
+dateButn.addEventListener("click", Date);
