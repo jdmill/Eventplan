c7c599df02d28649ea3a5c6909e072b09b73608c
sebastian Chunley <101592116+chuseblok@users.noreply.github.com>
2022-04-05 11:41:11 -0400

    Add files via upload

--- Event.js
+++ Event.js
@@ -5,6 +5,10 @@ var afternoonButn = document.getElementById("get-afternoon");
 var nightButn = document.getElementById("get-night");
 var searchButn = document.getElementById("get-search");
 var dateButn = document.getElementById("get-date");
+var eventNameButn = document.getElementById("get-event");
+var scrollArrowRightButn = document.getElementById("get-Right");
+var scrollArrowLeftButn = document.getElementById("get-Left");
+var scrollNumberButn = document.getElementById("get-number");
 
 concertButn.addEventListener("click", Concert);
 
@@ -19,3 +23,12 @@ nightButn.addEventListener("click", Night);
 searchButn.addEventListener("click", Search);
 
 dateButn.addEventListener("click", Date);
+
+eventNameButn.addEventListener("click", Event);
+
+scrollArrowRightButn.addEventListener("click", Right-Arrow);
+
+scrollArrowLeftButn.addEventListener("click", Left-Arrow);
+
+scrollNumberButn.addEventListener("click", Number);
+
